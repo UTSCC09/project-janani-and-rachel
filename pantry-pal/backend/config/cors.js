408063cd36de8027ce2900973e79f13e00c2447b
panrach <panrrachel@gmail.com>
2024-11-05 21:04:42 -0500

    add more stuff

--- pantry-pal/backend/config/cors.js
+++ pantry-pal/backend/config/cors.js
@@ -1,5 +1,4 @@
 // for cross domain requests
-
 const corsConfig = {
     origin: ['http://localhost:3000'],  // Allow requests from this origin (frontend)
     // todo: change the origin when this is deployed

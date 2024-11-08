25f330019d900a428b1e6e4621365ef2cd208300
Rachel Pan <126734747+panrach@users.noreply.github.com>
2024-11-08 14:52:29 -0500

    Update cors.js

--- pantry-pal/backend/config/cors.js
+++ pantry-pal/backend/config/cors.js
@@ -1,6 +1,6 @@
 // for cross domain requests
 const corsConfig = {
-    origin: ['http://localhost:3000', 'http://35.208.68.213'],  
+    origin: ['http://localhost:3000', 'http://rachelpan.me'],  
     // Allow requests from this origin (frontend)
     // second origin is the internal ip of our vm
     credentials: true, // Allow cookies to be sent from the frontend

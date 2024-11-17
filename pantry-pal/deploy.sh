f14a18f88ab8109267eefe9a71048d7088e6903c
panrach <panrrachel@gmail.com>
2024-11-16 22:34:22 -0500

    deploy script

--- pantry-pal/deploy.sh
+++ pantry-pal/deploy.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/sh
+echo "going to frontend directory"
+cd frontend
 echo "Stopping and removing existing frontend container..."
 echo "Building and running new frontend container..."
-docker build -t pp-frontend -f ./frontend/Dockerfile .
+docker build -t pp-frontend .
 
+echo "going out of frontend and into backend directory"
+cd ..
+cd backend
 echo "Stopping and removing existing backend container..."
 echo "Building and running new backend container..."
-docker build -t pp-backend -f ./backend/Dockerfile .
+docker build -t pp-backend .
 
+echo "going out of backend and into pantry-pal directory"
+cd ..
 docker compose down --remove-orphans
 docker rmi $(docker images --filter "dangling=true" -q --no-tru+nc)
 docker compose up -d
+

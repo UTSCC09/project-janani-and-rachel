9da9f3aec7ead5f4add4b4d37da8a80141666cd4
panrach <panrrachel@gmail.com>
2024-11-16 22:05:29 -0500

    docker files

--- pantry-pal/frontend/deploy.sh
+++ pantry-pal/frontend/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
-sudo docker stop pp-frontend-container
-sudo docker rm pp-frontend-container
-sudo docker build -t pp-frontend .
-sudo docker run -d -p 80:3000 --name pp-frontend-container pp-frontend
+echo "Stopping and removing existing frontend container..."
+docker stop pp-frontend-container
+docker rm pp-frontend-container
+echo "Pruning docker system..."
+docker system prune -f
+echo "Building and running new frontend container..."
+docker build -t pp-frontend .
+docker run -d -p 80:3000 --name pp-frontend-container pp-frontend

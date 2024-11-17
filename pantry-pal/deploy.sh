ead8455a9c8342a61e475a02ba3529aa7d117d07
panrach <panrrachel@gmail.com>
2024-11-16 22:27:30 -0500

    deploy script again

--- pantry-pal/deploy.sh
+++ pantry-pal/deploy.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/sh
 echo "Stopping and removing existing frontend container..."
-docker stop pp-frontend-container
-docker rm pp-frontend-container
-echo "Pruning docker system..."
-docker system prune -f
 echo "Building and running new frontend container..."
-docker build -t pp-frontend -f /frontend/Dockerfile .
+docker build -t pp-frontend -f ./frontend/Dockerfile .
 
 echo "Stopping and removing existing backend container..."
-docker stop pp-backend-container
-docker rm pp-backend-container
-echo "Pruning docker system..."
-docker system prune -f
 echo "Building and running new backend container..."
-docker build -t pp-backend -f /backend/Dockerfile .
+docker build -t pp-backend -f ./backend/Dockerfile .
 
 docker compose down --remove-orphans
 docker rmi $(docker images --filter "dangling=true" -q --no-tru+nc)

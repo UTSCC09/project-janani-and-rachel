20ab0f66c076bb5338f2b4ce5222250f9ec002cc
panrach <panrrachel@gmail.com>
2024-11-19 19:12:57 -0500

    change ingredients to pantry

--- pantry-pal/frontend/src/pages/index.js
+++ pantry-pal/frontend/src/pages/index.js
@@ -118,7 +118,7 @@ export default function Home() {
                   onClose={() => setAnchorEl(null)}
                 >
                   <MenuItem onClick={() => handleMenuClose("recipes")}>Recipes</MenuItem>
-                  <MenuItem onClick={() => handleMenuClose("ingredients")}>Ingredients</MenuItem>
+                  <MenuItem onClick={() => handleMenuClose("ingredients")}>My Pantry</MenuItem>
                   <MenuItem onClick={() => handleMenuClose("shoppingList")}>Shopping List</MenuItem>
                   <MenuItem onClick={() => handleMenuClose("calendar")}>Calendar</MenuItem>
                   <MenuItem onClick={() => handleMenuClose("recipeSearch")}>Recipe Search</MenuItem>

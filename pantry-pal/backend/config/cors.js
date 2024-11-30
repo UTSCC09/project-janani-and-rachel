ccfab2076ac2a5ef0de982d088ac2b911c5d61ce
Janani Gurram <68124448+JG-ctrl@users.noreply.github.com>
2024-11-30 00:06:39 -0500

    clean up comments

--- pantry-pal/backend/config/cors.js
+++ pantry-pal/backend/config/cors.js
@@ -1,7 +1,7 @@
 // for cross domain requests
 const corsConfig = {
     origin: [process.env.FRONTEND_URL],  
-    // Allow requests from this origin (frontend)
+    // allow requests from this origin (frontend)
     // second origin is the internal ip of our vm
     credentials: true, // Allow cookies to be sent from the frontend
 }
--- pantry-pal/backend/config/firebase.js
+++ pantry-pal/backend/config/firebase.js
@@ -1,23 +1,7 @@
-// backend/config/firebase.js
-//import { initializeApp } from 'firebase/app';
 import admin from 'firebase-admin';
-//import { getFirestore } from 'firebase-admin/firestore';
-//import { getAuth } from 'firebase/auth';
 import { readFileSync } from 'fs';
 
 const serviceAccount = JSON.parse(readFileSync(new URL('./pantry-pal-7ccff-firebase-adminsdk-wf4ap-e4d9fcdb25.json', import.meta.url)));
-
-
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-// };
 
 const firebaseAdminConfig = {
   credential: admin.credential.cert(serviceAccount),
